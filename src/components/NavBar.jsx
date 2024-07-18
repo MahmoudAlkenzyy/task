dbe04c4a8fab6a8af6909f4f10b8164edc6efad1
MahmoudAlkenzyy <105129954+MahmoudAlkenzyy@users.noreply.github.com>
2024-07-18 19:45:35 +0300

    #

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,8 +2,12 @@ export default function NavBar() {
   return (
     <>
       <nav className="navbar bg-body-tertiary">
-        <div className="container-fluid px-5">
-          <p className="h2  fw-bold">AK Bank</p>
+        <div className="container-fluid justify-content-center px-5 ">
+          <p className="h2  fw-bold ">
+            Welcome To
+            <span className="text-info px-1">Route</span>
+            Teach Titans
+          </p>
         </div>
       </nav>
     </>
